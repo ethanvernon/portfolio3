c5c04bcdeedf95c6be4af9521665e40a8ef955d2
Ethan Vernon <ethan.vernon@colorado.edu>
2020-08-09 02:34:07 -0500

    adds php req and axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,22 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  mounted() {
+    console.log('test')
+    axios.get('test.php')
+      .then(function (response) {
+         console.log('response:', response)
+      })
+      .catch(function (error) {
+         console.log(error);
+      });
   }
 }
 </script>
