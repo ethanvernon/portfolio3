82eb701b6944b9514001b2496a2351e4910ffc11
Ethan Vernon <ethan.vernon@colorado.edu>
2020-08-11 18:46:58 -0500

    clears out default

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <Home />
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import axios from 'axios';
+import Home from "@/components/Home.vue";
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  },
-  mounted() {
-    console.log('test')
-    axios.get('test.php')
-      .then(function (response) {
-         console.log('response:', response)
-      })
-      .catch(function (error) {
-         console.log(error);
-      });
-  }
-}
+    name: "Home",
+    components: {
+        Home
+    },
+    mounted() {
+        console.log("test");
+        axios
+            .get("test.php")
+            .then(function(response) {
+                console.log("response:", response);
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    }
+};
 </script>
